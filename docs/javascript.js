6e394639c6e158d42436aab6fa4bd1a23b3f735c
Martin Holtsmeier <MartinHolts@users.noreply.github.com>
2021-06-02 21:52:26 +0300

    Added files.

---
+++ docs/javascript.js
@@ -0,0 +1,7 @@
+/* When the user clicks on the menu button, toggle between hiding and showing the dropdown content */
+function openMenu() {
+	document.getElementById("dropdownContent").classList.toggle("show");
+}
+function openLogInWindow() {
+	document.getElementById("blackBackground").classList.toggle("show");
+}
